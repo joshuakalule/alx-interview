bb80aa1cbd59623e43e56b371b1add300cb5f631
Kalule Joshua <kaljoshkalule@gmail.com>
2024-06-30 18:46:30 +0000

    update files

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 """Pascal's triangle Implementation."""
 
+
+def value(idx, array):
+    """Helper function."""
+    if idx < 0 or idx >= len(array):
+        return 0
+    else:
+        return array[idx]
+
+
 def pascal_triangle(n):
     """Main function to execute pascal's triangle."""
     map = []
@@ -8,6 +17,16 @@ def pascal_triangle(n):
         return map
 
     map.append([1])
+    if (n == 1):
+        return map
 
-    for i in range(2, n, 1):
-        print(i)
+    for i in range(1, n):
+        row = []
+        prev_row = map[i - 1]
+        prev_row_len = len(prev_row)
+        for x in range(prev_row_len + 1):
+            num = value(x - 1, prev_row) + value(x, prev_row)
+            row.append(num)
+        map.append(row)
+
+    return map

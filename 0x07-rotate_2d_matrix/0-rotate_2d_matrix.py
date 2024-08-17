32bb7fa7fab4c475939fc02a3669209939746d78
Joshua Kalule <kaljoshkalule@gmail.com>
2024-08-17 05:08:41 +0300

    make rows lists not tuples

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,7 +6,7 @@ def rotate_2d_matrix(matrix: list) -> None:
     """Implement inplace 2D matrix rotation of 90°."""
     # 1. transpose matrix
     for i, new_row in enumerate(zip(*matrix)):
-        matrix[i] = new_row
+        matrix[i] = list(new_row)
     # 2. reverse rows
     for i in range(len(matrix)):
         matrix[i] = matrix[i][::-1]
